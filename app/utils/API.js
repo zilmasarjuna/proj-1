100f766ee8c8e1574519c1bb8c034808a97c2d9f
Zilmas Arjuna <zilmas@clappingape.com>
2019-12-02 12:52:59 +0700

    fix auth redux persist

--- app/utils/API.js
+++ app/utils/API.js
@@ -1,11 +1,45 @@
 import axios from 'axios'
+import config from 'app/config'
+import Browser from 'utils/Browser'
+import { getAccessToken, removeToken } from 'actions/Auth'
+import { mainPersistConfig } from 'store/configureStore'
+import { purgeStoredState } from 'redux-persist'
 
 const apiBaseURL = 'http://localhost:3000'
 
 axios.defaults.baseURL = apiBaseURL
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
 
-axios.interceptors.response.use(response => response, error => Promise.reject(error))
+axios.interceptors.request.use(
+  (response) => {
+    if (!response.headers.Authorization) {
+      const token = getAccessToken()
+
+      if (token) {
+        response.headers.Authorization = token
+      }
+    }
+
+    return response
+  },
+  error => Promise.reject(error),
+)
+
+axios.interceptors.response.use(
+  (response) => {
+    if (response.data.data) {
+      if (response.data.data.message === 'Token signature is expired') {
+        purgeStoredState(mainPersistConfig).then(() => {
+          removeToken()
+          Browser.setWindowHref('/login')
+        })
+      }
+    }
+
+    return response
+  },
+  error => Promise.reject(error),
+)
 
 export default class API {
   static get(path, options = {}) {
